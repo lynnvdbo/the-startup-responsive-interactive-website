10463bd679fc54d62b93a609ad1cea75ef14dce6
lynnvdbo <lynn.van.den.bosch@hva.nl>
2026-01-15 22:47:59 +0100

    learn-more knop leuke animatie toegevoegd #7

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,20 @@ function toggleNav() {
     headerNav.classList.toggle('is-open')
 }
 
+
+// Learn-more-about-270
+// Stap 1: querySelector
+let theLink = document.querySelector('.Learn-more-about-270')
+
+// Stap 2: addEventListener
+theLink.addEventListener("mouseout", cirkleHandler)
+
+// Stap 3: (Callback functie met) classList (.toggle(), .add(), etc.)
+// scaleLink.classList.toggle..
+function cirkleHandler() {
+  theLink.classList.add ("rotate")
+}
+
+theLink.addEventListener("animationend", function(){
+  theLink.classList.remove("rotate")
+})
